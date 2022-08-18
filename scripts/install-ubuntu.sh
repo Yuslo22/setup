b00eb7726e701f67123448c284e97b8f90546d98
Yuslo22 <niklas.scholz@mail.de>
2022-08-18 21:30:57 +0200

    fix command order

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -4,7 +4,7 @@
 sudo apt update
 # Install Dependencies
 sudo apt install -y git yadm zsh wget unzip
-# Change default shell to zsh
-sudo chsh -s $(which zsh) $(whoami)
 # Clone dotfiles
 yadm clone https://github.com/Yuslo22/dotfiles.git --bootstrap
+# Change default shell to zsh
+sudo chsh -s $(which zsh) $(whoami)
